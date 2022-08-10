40bf73a72a178fc43e9ff3c440ac3ae227a6fa77
Takhyun Kim <kimkih1218@gmail.com>
2022-08-10 21:42:41 +0900

    feat: apply selected nav style

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import navLinks from "../../common/routes";
 
 import styles from "./Nav.module.css";
 
 const Nav = () => {
+  const { pathname } = useRouter();
+
   return (
     <nav className={styles.wrapper}>
       {navLinks.map((nav) => (
         <Link href={nav.link} key={nav.title}>
-          <a className={styles.nav_text}> {nav.title}</a>
+          <a
+            className={
+              pathname.includes(nav.link)
+                ? styles.nav_text_clicked
+                : styles.nav_text
+            }
+          >
+            {nav.title}
+          </a>
         </Link>
       ))}
     </nav>
