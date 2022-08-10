46b5ed23dbf05083750095d1dc3962f5e8d94d67
Takhyun Kim <kimkih1218@gmail.com>
2022-08-10 21:17:48 +0900

    feat: apply default nav style

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -2,12 +2,14 @@ import Link from "next/link";
 
 import navLinks from "../../common/routes";
 
+import styles from "./Nav.module.css";
+
 const Nav = () => {
   return (
-    <nav>
+    <nav className={styles.wrapper}>
       {navLinks.map((nav) => (
         <Link href={nav.link} key={nav.title}>
-          <a>{nav.title}</a>
+          <a className={styles.nav_text}> {nav.title}</a>
         </Link>
       ))}
     </nav>
