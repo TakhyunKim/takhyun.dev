a06160c2a8a7551a13edb3c97c7c93ae64df4272
Takhyun Kim <kimkih1218@gmail.com>
2022-08-14 19:00:37 +0900

    feat: apply test post List data to Blog page and PostList component

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,16 +1,33 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
+
+import { getSortedPostsData } from "../lib/posts";
+import type { PostData } from "../lib/posts";
 
 import Container from "../components/Container";
 import PostList from "../components/Blog/PostList";
 import RecommendPost from "../components/Blog/RecommendPost";
 
-const Blog: NextPage = () => {
+interface BlogProps {
+  allPostsData: PostData[];
+}
+
+const Blog: NextPage<BlogProps> = ({ allPostsData }) => {
   return (
     <Container>
       <RecommendPost />
-      <PostList />
+      <PostList allPostsData={allPostsData} />
     </Container>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 };
 
 export default Blog;
