38e2915065c53bdc324b67aeafee34cfaded3c0d
Takhyun Kim <kimkih1218@gmail.com>
2022-08-23 03:43:16 +0900

    refactor: moved handlePostClick to PostList from PostItem

--- components/PostList/index.tsx
+++ components/PostList/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 
-import { PostData } from "../../lib/posts";
+import type { PostData } from "../../lib/posts";
+import type { MarkdownPost } from "../../common/types/markdownPost";
 
 import PostItem from "./PostItem";
 
@@ -8,15 +10,27 @@ import styles from "./PostList.module.css";
 
 interface PostListProps {
   postList: PostData[];
+  postType: MarkdownPost;
 }
 
-const PostList: NextPage<PostListProps> = ({ postList }) => {
+const PostList: NextPage<PostListProps> = ({ postType, postList }) => {
+  const router = useRouter();
+
   return (
     <div>
       <div className={styles.post_list_wrapper}>
-        {postList.map((postData) => (
-          <PostItem key={postData.id} postData={postData} />
-        ))}
+        {postList.map((postData) => {
+          const redirectToPost = () => {
+            router.push(`/${postType}/${postData.id}`);
+          };
+          return (
+            <PostItem
+              key={postData.id}
+              postData={postData}
+              onClick={redirectToPost}
+            />
+          );
+        })}
       </div>
     </div>
   );
