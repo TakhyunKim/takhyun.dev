a7ef940948d41c3be4291d205c2d7385e0843e2f
Takhyun Kim <kimkih1218@gmail.com>
2022-08-30 15:40:34 +0900

    chore: remove packages and code about styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,10 @@
 import type { AppProps } from "next/app";
-import { useEffect, useState } from "react";
-import { ThemeProvider } from "styled-components";
-
-import { lightTheme, darkTheme, GlobalStyles } from "./styles/theme";
-
-import { getInitialTheme } from "../lib/getInitialTheme";
 
 import "../styles/globals.css";
 
 // This default export is required in a new `pages/_app.js` file.
 function MyApp({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState<string>("dark");
-
-  useEffect(() => {
-    const initialTheme = getInitialTheme();
-
-    setTheme(initialTheme);
-  }, [theme]);
-
-  return (
-    <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
