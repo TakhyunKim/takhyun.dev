7b70b7f72a559d092d644ffcc50221fc59433ef1
Takhyun Kim <kimkih1218@gmail.com>
2022-08-30 11:00:31 +0900

    feat: apply dark, light mode to App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,29 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "styled-components";
+
+import { lightTheme, darkTheme, GlobalStyles } from "./styles/theme";
+
+import { getInitialTheme } from "../lib/getInitialTheme";
+
+import "../styles/globals.css";
 
 // This default export is required in a new `pages/_app.js` file.
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [theme, setTheme] = useState<string>("dark");
+
+  useEffect(() => {
+    const initialTheme = getInitialTheme();
+
+    setTheme(initialTheme);
+  }, [theme]);
+
+  return (
+    <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
