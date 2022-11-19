329ce88178001ba6cc04ac7ab08b47211473abad
Takhyun Kim <kimkih1218@gmail.com>
2022-11-19 12:38:22 +0900

    feat: add 404 page component and style

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,49 @@
-const NotFound = () => {
-  return <q>404</q>;
+import type { NextPage, GetStaticProps } from "next";
+import { NextSeo } from "next-seo";
+import Image from "next/image";
+
+import { getSortedPostsAndProjectsData } from "../lib/posts";
+import type { PostData } from "../lib/posts";
+
+import NotFoundHeader from "../components/NotFound/NotFoundHeader";
+import RecentPosts from "../components/Home/RecentPosts";
+
+import styles from "./styles/notFound.module.css";
+
+interface NotFoundProps {
+  recentPosts: PostData[];
+}
+
+const NotFound: NextPage<NotFoundProps> = ({ recentPosts }) => {
+  return (
+    <>
+      <NextSeo
+        title="Takhyun Kim & Frontend Engineer"
+        description="프론트엔드 개발자 김탁현의 기술 블로그"
+      />
+      <div className={styles.header_wrapper}>
+        <div className={styles.intro_profile}>
+          <Image
+            src="/images/intro-profile.jpg"
+            layout="fill"
+            alt="intro profile"
+          />
+        </div>
+        <NotFoundHeader />
+      </div>
+      <RecentPosts postList={recentPosts} />
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const recentPosts = getSortedPostsAndProjectsData().slice(0, 3);
+
+  return {
+    props: {
+      recentPosts,
+    },
+  };
 };
 
 export default NotFound;
