5d2c83bc08334b2619d494332ee460aa997affd1
TakhyunKim <kimkih1218@gmail.com>
2024-04-09 23:08:55 +0900

    feat: add algorithms navigation link

--- common/routes/index.ts
+++ common/routes/index.ts
@@ -3,6 +3,7 @@ type NavLink = { title: string; link: string };
 const navLinks: NavLink[] = [
   { title: "Posts", link: "/posts" },
   { title: "Projects", link: "/projects" },
+  { title: "algorithms", link: "/algorithms" },
 ];
 
 export default navLinks;
